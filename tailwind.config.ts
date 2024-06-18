0a3168ca6e19f84535ad2be2b59fe4c808902da1
sinskiy <sinskiy@proton.me>
2024-06-18 12:31:17 +0300

    feat: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      background: "var(--background)",
+      surface: "var(--surface)",
+      primary: "var(--primary)",
+      text: {
+        DEFAULT: "var(--text)",
+        variant: "var(--text-variant)",
+        primary: "var(--primary)",
       },
     },
   },
