d6a6be228ecabfc12a0966eb165cdb5a8cdfe0df
sinskiy <sinskiy@proton.me>
2024-06-18 12:57:18 +0300

    feat: style daily note section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 export default function Home() {
   return (
-    <main>
+    <main className="px-4 py-8 grid gap-8">
       <section>
         <h2>Daily note</h2>
-        <form action="" method="get">
+        <form action="" method="get" className="grid gap-2">
           <textarea
             name="daily"
             id="daily"
+            className="card"
+            rows={6}
             placeholder="Today I accomplished a lot. I did this and this"
           ></textarea>
-          <button type="submit">Save</button>
+          <button type="submit" className="justify-self-end">
+            Save
+          </button>
         </form>
       </section>
-      <section>
+      {/* <section>
         <h2>Previous days</h2>
         <input type="date" name="date" id="date" />
         <h3>Note</h3>
@@ -21,7 +25,7 @@ export default function Home() {
           contradiction, check your premises. You will find that one of them is
           wrong.
         </p>
-      </section>
+      </section> */}
     </main>
   );
 }
