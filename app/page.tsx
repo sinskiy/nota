c3663df0d15a4a3b37ae3cd4fed239d4204c79d4
sinskiy <sinskiy@proton.me>
2024-06-18 12:39:48 +0300

    feat: add html structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
 export default function Home() {
-  return <h1>hello world</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Daily note</h2>
+        <form action="" method="get">
+          <textarea
+            name="daily"
+            id="daily"
+            placeholder="Today I accomplished a lot. I did this and this"
+          ></textarea>
+          <button type="submit">Save</button>
+        </form>
+      </section>
+      <section>
+        <h2>Previous days</h2>
+        <input type="date" name="date" id="date" />
+        <h3>Note</h3>
+        <p>
+          Contradictions do not exist. Whenever you think that you are facing a
+          contradiction, check your premises. You will find that one of them is
+          wrong.
+        </p>
+      </section>
+    </main>
+  );
 }
