0e2b0b1cf55550ee0883eeaa825db61d4279c7a5
sinskiy <sinskiy@proton.me>
2024-06-20 09:17:05 +0300

    feat: add ability to add and delete multiple templates

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,33 +1,60 @@
 import { useEffect, useState } from "react";
+import SettingsTemplate from "./SettingsTemplate";
 
 interface Props {}
 
+export type SetTemplate = (newTemplate: string, templateIndex: number) => void;
+export type DeleteTemplate = (templateIndex: number) => void;
+
 export default function Settings({}: Props) {
-  const [template, setTemplate] = useState("");
+  const [templates, setTemplates] = useState<string[]>([]);
   useEffect(() => {
-    const savedTemplate = localStorage.getItem("template");
-    savedTemplate && setTemplate(savedTemplate);
+    const savedTemplates = localStorage.getItem("templates");
+    savedTemplates && setTemplates(JSON.parse(savedTemplates));
   }, []);
-  function saveTemplate() {
-    localStorage.setItem("template", template);
+  function saveTemplates() {
+    localStorage.setItem("templates", JSON.stringify(templates));
   }
+
+  const setTemplate: SetTemplate = (newTemplate, templateIndex) => {
+    const newTemplates = templates.map((template, i) => {
+      if (i === templateIndex) {
+        return newTemplate;
+      } else {
+        return template;
+      }
+    });
+    setTemplates(newTemplates);
+  };
+  function addTemplate() {
+    setTemplates([...templates, ""]);
+  }
+  const deleteTemplate: DeleteTemplate = (templateIndex) => {
+    setTemplates(templates.filter((template, i) => templateIndex !== i));
+  };
   return (
     <div className="card p-8">
       <section>
         <header>
           <h2>Template</h2>
         </header>
-        <textarea
-          value={template}
-          onChange={(e) => setTemplate(e.target.value)}
-          className="card bg-onSurface w-full"
-          rows={6}
-          name="template"
-          id="template"
-        ></textarea>
+        <ul>
+          {templates.map((template, i) => (
+            <SettingsTemplate
+              template={template}
+              index={i}
+              setTemplate={setTemplate}
+              deleteTemplate={deleteTemplate}
+              key={i}
+            />
+          ))}
+        </ul>
+        <button className="text-button mt-4" onClick={addTemplate}>
+          Add new template
+        </button>
       </section>
       <form action="" method="dialog" className="flex gap-4 mt-8">
-        <button onClick={saveTemplate} className="text-button">
+        <button onClick={saveTemplates} className="text-button">
           Save
         </button>
         <button className="text-button">Close</button>
---
+++ components/SettingsTemplate.tsx
@@ -0,0 +1,39 @@
+import { DeleteTemplate, SetTemplate } from "./Settings";
+
+interface Props {
+  template: string;
+  index: number;
+  setTemplate: SetTemplate;
+  deleteTemplate: DeleteTemplate;
+}
+
+export default function SettingsTemplate({
+  template,
+  index,
+  setTemplate,
+  deleteTemplate,
+}: Props) {
+  return (
+    <li className="mt-2">
+      <textarea
+        value={template}
+        onChange={(e) => setTemplate(e.target.value, index)}
+        className="card bg-onSurface w-full"
+        rows={6}
+        name="template"
+        id="template"
+      ></textarea>
+      <button onClick={() => deleteTemplate(index)} className="icon-button">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="currentColor"
+        >
+          <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+        </svg>
+      </button>
+    </li>
+  );
+}
